9e971f38afda7efcfddec5e7058daa5b46b5155c
Yahavcd <56316974+Yahavcd@users.noreply.github.com>
2021-06-27 14:54:24 +0300

    Minor Tweaks

--- src/EndGame.gd
+++ src/EndGame.gd
@@ -7,7 +7,15 @@ func _ready():
 	$Control/VBoxContainer/Score.text = "Your Score: " + String(points)
 
 func _on_Button_pressed():
+	init()
+	get_tree().change_scene("res://Scenes/Boards/BoredTemplet.tscn")
+
+
+func _on_Button2_pressed():
+	init()
+	get_tree().change_scene("res://Scenes/UI/StartGame.tscn")
+
+func init():
 	gamestate.points = 0
 	gamestate.lives = 1
 	gamestate.is_hit = false
-	get_tree().change_scene("res://Scenes/Boards/BoredTemplet.tscn")
